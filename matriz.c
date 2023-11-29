e0134e28bfadd53134f4892a0f153fce6c8984bd
Giovanny Padilla <giovanny.padilla@udla.edu.ec>
2023-11-29 16:02:47 -0500

    Uso de srand para generar numeros aleatorios para la matriz

--- matriz.c
+++ matriz.c
@@ -5,13 +5,38 @@
 int main(int argc, char const *argv[])
 {
     int fila, columna;
-    printf("Ingrese el numero de filas: ");
+    printf("Ingrese el numero de filas:\n");
     scanf("%d", &fila);
-    printf("Ingrese el numero de columnas: ");
+    printf("Ingrese el numero de columnas:\n");
     scanf("%d", &columna);
 
     int A[fila][columna]; 
     int B[fila][columna];
 
-     return 0;
+//usamos srand para generar numeros leatorios para la matriz
+    srand(time(NULL));
+
+    printf("\nMatriz A:\n");
+
+    for (int i = 0; i < fila; i++){
+        for (int j = 0; j < columna; j++)
+        {
+            A[i][j] = rand() % 100; // Generar un número aleatorio entre 0 y 99
+            printf("%d ", A[i][j]);
+            
+        }
+        printf("\n");
+}
+    printf("\nMatriz B:\n");
+    for (int i = 0; i < fila; i++){
+        for (int j = 0; j < columna; j++)
+        {
+            B[i][j] = rand() % 100; // Generar un número aleatorio entre 0 y 99
+            printf("%d ", B[i][j]);
+            
+        }
+        printf("\n");
+}
+    
+    return 0;
 }
