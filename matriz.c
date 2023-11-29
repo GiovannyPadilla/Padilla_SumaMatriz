73ce504fc7727f4f7258cdbbaa16552ebd2796e5
Laboratorio <laboratorio@udla-ec.int>
2023-11-29 15:49:03 -0500

    Creacion de varibles

---
+++ matriz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    int fila, columna;
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &fila);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &columna);
+
+    int A[fila][columna]; 
+    int B[fila][columna];
+
+     return 0;
+}
